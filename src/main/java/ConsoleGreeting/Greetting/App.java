1d1a0dd246a49cd1d71c10f925e00f369b5da7e1
Oleksandr Ryzhkov <ryzhman@gmail.com>
2016-02-23 13:28:44 +0200

    Enhancing modularity and Unittests

--- src/main/java/ConsoleGreeting/Greetting/App.java
+++ src/main/java/ConsoleGreeting/Greetting/App.java
@@ -7,9 +7,14 @@ import java.util.ResourceBundle;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class App 
-{
+public class App{
 	private static final Logger log = LogManager.getLogger();
+	private static final String RESOURCEFILE = "messages";
+	private static ResourceBundle resource;
+	private static final LocalTime sixAm = LocalTime.of(6,0);
+	private static final LocalTime nineAm = LocalTime.of(9,0);
+	private static final LocalTime sevenPm = LocalTime.of(19,0);
+	private static final LocalTime elevenPm = LocalTime.of(23,0);
 
     public static void main( String[] args )
     {
@@ -18,43 +23,57 @@ public class App
     	System.out.println(greet(time));
     }
     
+    
     public static String greet(LocalTime time){
-    	if(time==null){
-    		log.warn("Invalid argument for greet(). Return null");
-    		return "Unsupported type";
-    	}
-
-    	final String RESOURCEFILE = "messages";
-		String timeOfDay = "";
-    	ResourceBundle resource = null;
-    	try{
-    		resource = ResourceBundle.getBundle(RESOURCEFILE);
-    	}catch(MissingResourceException e){
-    		e.getMessage();
-    	   	log.error("Exception occured " + e.getMessage());
-    	}
+    	isTimeValid(time);
+    	return checkTime(time);
+    }
+    
+    public static String checkTime(LocalTime time){
+    	String timeOfDay = "";
+    	ResourceBundle resource = getResource();
     	
     	try{
-    		if((time.getHour()>6)&&(time.getHour()<9)){
+    		if(time.isAfter(sixAm)&&time.isBefore(nineAm)||(time.equals(LocalTime.of(6,0)))||(time.equals(LocalTime.of(9,0)))){
     			timeOfDay=resource.getString("morning");
-        		log.info("Invocation of method displayed " + timeOfDay);
+    			log.info("Invocation of method displayed " + timeOfDay);
     			return timeOfDay;
-    		}else if(time.getHour()>9&&(time.getHour()<19)){
+    		}else if(time.isAfter(nineAm)&&time.isBefore(sevenPm)||time.equals(LocalTime.of(19,0))){
     			timeOfDay= resource.getString("day");
-        		log.info("Invocation of method displayed " + timeOfDay);
+    			log.info("Invocation of method displayed " + timeOfDay);
     			return timeOfDay;
-    		}else if((time.getHour()>19)&&(time.getHour()<23)){
+    		}else if(time.isAfter(sevenPm)&&time.isBefore(elevenPm)||time.equals(LocalTime.of(23,0))){
     			timeOfDay = resource.getString("evening");
-        		log.info("Invocation of method displayed " + timeOfDay);
+    			log.info("Invocation of method displayed " + timeOfDay);
     			return timeOfDay;
     		}else{
     			timeOfDay = resource.getString("night");
-        		log.info("Invocation of method displayed " + timeOfDay);
+    			log.info("Invocation of method displayed " + timeOfDay);
     			return timeOfDay;
     		}
+    	}catch(NullPointerException e){
+    		log.error("Exception occured " + e.getMessage());
+    	   	return null;
+    	}
+    }
+    
+    public static ResourceBundle getResource(){
+		try{
+    		resource = ResourceBundle.getBundle(RESOURCEFILE);
+    		return resource;
     	}catch(MissingResourceException e){
+    		e.getMessage();
     	   	log.error("Exception occured " + e.getMessage());
     	   	return null;
     	}
+	}
+    
+    public static String isTimeValid(LocalTime time){
+    	if(time==null){
+    		log.warn("Invalid argument for greet(). Return null");
+    		String notValid = getResource().getString("null");
+    		return notValid;
+    	}
+		return null;
     }
 }
--- src/test/java/ConsoleGreeting/Greetting/AppTest.java
+++ src/test/java/ConsoleGreeting/Greetting/AppTest.java
@@ -1,6 +1,7 @@
 package ConsoleGreeting.Greetting;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.time.LocalTime;
 import java.util.ResourceBundle;
@@ -8,35 +9,41 @@ import java.util.ResourceBundle;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import ConsoleGreeting.Greetting.App;
-
 
 public class AppTest{
-    
 	private static LocalTime morningTestTime;
 	private static LocalTime dayTestTime;
 	private static LocalTime eveningTestTime;
 	private static LocalTime nightTestTime;
+	private static LocalTime incorrectTime1;
 	private final static String RESOURCEFILE = "messages";
 	private static ResourceBundle resource;
 
-
 	@BeforeClass
     public static void init(){
-		morningTestTime = LocalTime.of(7, 00);
-		dayTestTime = LocalTime.of(14, 00);
-		eveningTestTime = LocalTime.of(20, 00);
+		morningTestTime = LocalTime.of(9, 00);
+		dayTestTime = LocalTime.of(19, 00);
+		eveningTestTime = LocalTime.of(23, 00);
 		nightTestTime = LocalTime.of(1, 00);
 		resource = ResourceBundle.getBundle(RESOURCEFILE);
 	}
-
+	
 	@Test
-	public void testGreeting()
+	public void testCheckTime()
     {
-    	assertEquals(App.greet(morningTestTime), resource.getString("morning"));
-    	assertEquals(App.greet(dayTestTime), resource.getString("day"));
-    	assertEquals(App.greet(eveningTestTime), resource.getString("evening"));
-    	assertEquals(App.greet(nightTestTime), resource.getString("night"));
-    	
+    	assertEquals(App.checkTime(morningTestTime), resource.getString("morning"));
+    	assertEquals(App.checkTime(dayTestTime), resource.getString("day"));
+    	assertEquals(App.checkTime(eveningTestTime), resource.getString("evening"));
+    	assertEquals(App.checkTime(nightTestTime), resource.getString("night"));
     }
+	
+	@Test
+	public void testIsTimeValid(){
+		assertEquals(App.isTimeValid(incorrectTime1), resource.getString("null"));
+	}
+	
+	@Test
+	public void testGetResource(){
+		assertNotNull(App.getResource());
+	}
 }
